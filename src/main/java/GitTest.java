f5fea8352c4fa45090e8a433b51054d2eee3a4a4
ZhangHeng <ZhangHeng@atguigu>
2021-04-15 22:47:51 +0800

    hot-fix 1 commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello git");
+        System.out.println("hello hot-fix");
+        System.out.println("冲突 hot-fix");
     }
 }
