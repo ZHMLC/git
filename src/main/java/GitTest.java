e9117041ed6c54b680df8ce14c92adb378b87c77
ZhangHeng <ZhangHeng@atguigu>
2021-04-15 22:45:58 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello git");
+        System.out.println("hello 1116");
+        System.out.println("冲突master");
     }
 }
